778fd4f4d5a1c647496ea4689925f27a50462a54
Emily V <evanauken@students.stonehill.edu>
2016-12-04 20:42:23 -0500

    Update CollegeCluster.R

--- CollegeCluster.R
+++ CollegeCluster.R
@@ -20,31 +20,34 @@ NE$COSTT4_A <- as.numeric(NE$COSTT4_A)
 NE$PCTPELL <- as.numeric(NE$PCTPELL)
 NE$PCTFLOAN <- as.numeric(NE$PCTFLOAN)
 
-set.seed(2342)
+set.seed(7957)
 
-splitSample <- sample(1:2, size=nrow(NE),replace=TRUE,prob=c(0.7,0.3))
+splitSample <- sample(1:3, size=nrow(NE),replace=TRUE,prob=c(0.2,0.2,0.6))
 
 train <- NE[splitSample==1,]
 test <- NE[splitSample==2,]
 
-train2 <- select(train, LOCALE, UGDS, ADM_RATE, C150_4, SAT_AVG, UGDS_BLACK,UGDS_WHITE, UGDS_HISP, UGDS_ASIAN,UGDS_AIAN, UGDS_AIAN, RELAFFIL, COSTT4_A, PCTPELL,PCTFLOAN)
+train2 <- select(train, LOCALE, UGDS, ADM_RATE, C150_4, SAT_AVG, UGDS_BLACK,UGDS_WHITE, UGDS_HISP, UGDS_ASIAN,UGDS_AIAN, UGDS_AIAN, RELAFFIL, COSTT4_A, PCTPELL,PCTFLOAN,INSTNM)
 
-test2 <- select(test, LOCALE, UGDS, ADM_RATE, C150_4, SAT_AVG, UGDS_BLACK,UGDS_WHITE, UGDS_HISP, UGDS_ASIAN,UGDS_AIAN, UGDS_AIAN, RELAFFIL, COSTT4_A, PCTPELL,PCTFLOAN)
+test2 <- select(test, LOCALE, UGDS, ADM_RATE, C150_4, SAT_AVG, UGDS_BLACK,UGDS_WHITE, UGDS_HISP, UGDS_ASIAN,UGDS_AIAN, UGDS_AIAN, RELAFFIL, COSTT4_A, PCTPELL,PCTFLOAN, INSTNM)
 
 # Hierarchical clustering with Dendogram
+N <- train2
 
 #calculate Euclidean distance
-NE.dist <- dist(NE)
+N.dist <- dist(N)
 
-NE.hclust <- hclust(NE.dist)
+N.hclust <- hclust(N.dist)
 
-plot(NE.hclust,labels=NE$INSTNM,main='Dendogram of US Colleges', hang=-1)
+plot(N.hclust,labels=N$INSTNM,main='Dendogram of US Colleges', hang=-1)
 
 #Create a vector showing the cluster5 groups
 
-groups.5<- cutree(NE.hclust,5)  # repeat for 4 groups
+groups.5<- cutree(N.hclust,5)  # repeat for 4 groups
 table (groups.5)
-rect.hclust(NE.hclust, k=5, border="blue") 
+rect.hclust(N.hclust, k=5, border="blue") 
 
-write.table(rect.hclust, row.names=NE$INSTNM, sep=groups.5)
+write.table(rect.hclust, col.names=N$INSTNM, sep=groups.5)
+N <-cbind(N, groups.5)
+write.csv(N,"NEAcad.csv")
 
